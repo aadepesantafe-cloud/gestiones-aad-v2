73df9af3b48c70647c949cf47f4a0cdac896c616
aadepesantafe-cloud <aadepesantafe@gmail.com>
2026-08-13 16:07:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
 // Tiene esta forma:
 // https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXX/exec
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxcentnzzQUVGdBWcgixCCju71Q-UrDtblH-xdsde2UgtBwPZpoc6fndTPpAzhet33c/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxcentnzzQUVGdBWcgixCCju71Q-UrDtblH-xdsde2UgtBwPZpoc6fndTPpAzhet33c/exec"; 
